6bf045caf6c4f8b3365d433b198876b1ac627011
gaurav459433 <gaurav459433@gmail.com>
2022-11-12 18:29:43 +0530

    Employee FullTime Salary Logic

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -8,11 +8,14 @@ public class EmployeeWageProblem {
 
         Random random = new Random();
         int a = random.nextInt(2);
-        if(a == 1)
+        int wagePH = 20;
+        int FulldayH = 8;
+        if (a == 1) {
             System.out.println("Employee is Present");
-        else
+            System.out.println("Employee is Present\nPer day Wage is: " + (wagePH * FulldayH));
+        } else
             System.out.println("Employee is Absent");
+        System.out.println("Employee is Absent\nPer day Wage is: 0");
 
     }
-
 }
