836c0b6243ca0b71687c5fc390eb2a8eeaf7f025
gaurav459433 <gaurav459433@gmail.com>
2022-11-12 17:52:04 +0530

    Welcome To Employee Wage Computation

---
+++ EmployeeWageProblem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWageProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+    }
+
+}
