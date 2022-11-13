cf13529c2d712d99f494ddbbafe408cc15c96902
gaurav459433 <gaurav459433@gmail.com>
2022-11-13 17:17:28 +0530

    SwitchCase Salary Logic

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -17,5 +17,13 @@ public class EmployeeWageProblem {
             System.out.println("Employee is Present Halfday\nPer day Wage is: "+(wagePH*ParttimedayH));
         else
             System.out.println("Employee is Absent\nPer day Wage is: 0");
+        switch(a) {
+            case 0: System.out.println("Employee is Absent\nPer day Wage is: 0");
+                break;
+            case 1: System.out.println("Employee is Present\nPer day Wage is: "+(wagePH*FulldayH));
+                break;
+            case 2: System.out.println("Employee is Present Halfday\nPer day Wage is: "+(wagePH*ParttimedayH));
+                break;
+        }
     }
 }
